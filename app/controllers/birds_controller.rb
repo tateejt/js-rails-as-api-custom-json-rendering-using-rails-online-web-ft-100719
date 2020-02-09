f9f228635af1f7f81fb4d330f784eeef4f4275e4
Emery Tate <tateejt@gmail.com>
2020-02-09 19:42:09 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,8 @@ class BirdsController < ApplicationController
   
   def show 
     bird = Bird.find_by(id: params[:id])
+    if bird 
+      
     render json: birds.to_json(except: [:created_at, :updated_at])
   end
 end
